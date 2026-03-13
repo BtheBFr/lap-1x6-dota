88d03f7872849c9cb272a1b6a930457562b09fd2
BtheBFr <rtfmqwertu29v@gmail.com>
2026-03-13 21:20:00 +0500

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const CONFIG = {
     // API Google Apps Script
     API_URL: 'https://script.google.com/macros/s/AKfycbwR8PJEEXPid3KUezDwPxbAQILlIWJjE2iIWaXC0eJhjQV66kb8NjpRqi9Uz6PhAVpJVg/exec',
     
-    // GitHub raw URL
+    // GitHub raw URL (измени на свой)
     GITHUB_URL: 'https://raw.githubusercontent.com/BtheBFr/Lap-1x6-dota/main/',
     
     // Telegram Bot
@@ -19,16 +19,6 @@ const CONFIG = {
         R: 'R'
     },
     
-    // Настройки кеша (в часах)
-    CACHE: {
-        HEROES: 168,        // 7 дней
-        USER_DATA: 24,      // 1 день
-        SETTINGS: 24        // 1 день
-    },
-    
-    // Версия приложения
-    VERSION: '1.0.0'
+    // Версия
+    VERSION: '1.0.1'
 };
-
-// Проверка подключения
-console.log('Lap 1x6 Pack v' + CONFIG.VERSION + ' загружен');
