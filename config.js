fa11488f23e31cac28034ebfbc8a521d258676e5
BtheBFr <rtfmqwertu29v@gmail.com>
2026-03-14 14:23:30 +0500

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-// Конфигурация
-const CONFIG = {
-    API_URL: 'https://script.google.com/macros/s/AKfycbwR8PJEEXPid3KUezDwPxbAQILlIWJjE2iIWaXC0eJhjQV66kb8NjpRqi9Uz6PhAVpJVg/exec',
-    GITHUB_URL: 'https://raw.githubusercontent.com/BtheBFr/Lap-1x6-dota/main/',
-    BOT_URL: 'https://t.me/Lap1x6Dota_bot',
-    DEFAULT_BINDS: {
-        Q: 'Q', W: 'W', E: 'E', D: 'D', F: 'F', R: 'R'
-    }
-};
