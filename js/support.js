b179c2563813902442b79933cc26f0af7a7cae17
BtheBFr <rtfmqwertu29v@gmail.com>
2026-03-14 14:39:29 +0500

    Update support.js

--- js/support.js
+++ js/support.js
@@ -1,5 +1,5 @@
 // Отправка жалоб в Google Sheets
-// Apps Script URL - ЗАМЕНИТЕ НА СВОЙ!
+// Apps Script URL - ВАША ССЫЛКА
 const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyll3lmKAoU4xllrdtSzZAKXTWBum6ey-_2Euqp1qnkOaIvPClYKqxe73PfQhy-_5eTEQ/exec';
 
 // Отправка тикета в поддержку
@@ -42,14 +42,13 @@ function submitSupportTicket(problemType, hero, skill, description) {
     // Отправляем через Apps Script
     fetch(APPS_SCRIPT_URL, {
         method: 'POST',
-        mode: 'no-cors', // Важно для Google Apps Script
+        mode: 'no-cors',
         headers: {
             'Content-Type': 'application/json',
         },
         body: JSON.stringify(data)
     })
     .then(() => {
-        // Даже если no-cors, мы не получим ответ, но считаем что отправилось
         showToast('Спасибо! Жалоба отправлена');
         
         // Закрываем модалку
@@ -66,28 +65,5 @@ function submitSupportTicket(problemType, hero, skill, description) {
     });
 }
 
-// Альтернативный способ - открыть форму в новом окне (на случай если fetch не работает)
-function openSupportForm(problem, hero, skill, description) {
-    // Создаем URL с параметрами
-    const params = new URLSearchParams({
-        problem: problem,
-        hero: hero || '',
-        skill: skill || '',
-        description: description
-    });
-    
-    // Открываем в маленьком окне
-    window.open(
-        `https://docs.google.com/forms/d/e/1FAIpQLSdXXXXXXXXXX/viewform?${params.toString()}`,
-        'support',
-        'width=600,height=600'
-    );
-}
-
 // Сохраняем функцию глобально
 window.submitSupportTicket = submitSupportTicket;
-
-// Функция для ручного тестирования
-function testSupportTicket() {
-    submitSupportTicket('test', 'Luna', 'Eclipse', 'Тестовое сообщение');
-}
